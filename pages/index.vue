55339e7daa917e8e84d87e9c3f2cedf13f1f51a1
Jack Boehme <easycollen@gmail.com>
2025-04-22 18:14:31 +0900

    2

--- pages/index.vue
+++ pages/index.vue
@@ -50,6 +50,8 @@
 
 <script setup>
 import { ref } from "vue";
+import { useHead } from "nuxt/app";
+
 import ShowCardComponent from "~/components/ShowCard.vue";
 import AddFoodComponent from "~/components/AddFood.vue";
 import SetGoalComponent from "~/components/SetGoal.vue";
@@ -57,7 +59,15 @@ import TagManagementComponent from "~/components/TagManagement.vue";
 import HistoryRecordComponent from "~/components/HistoryRecord.vue";
 import FrozenCalculationComponent from "~/components/FrozenCalculation.vue";
 import PackagedCalculationComponent from "~/components/PackagedCalculation.vue";
-
+useHead({
+    meta: [
+        {
+            name: "viewport",
+            content:
+                "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no",
+        },
+    ],
+});
 // 定义标签数据
 const tabs = [
     { name: "添加食物", component: AddFoodComponent },
