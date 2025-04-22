59925ef3daf6a3787197040192c069686dfe6966
Jack Boehme <easycollen@gmail.com>
2025-04-22 14:22:13 +0900

    logo和飘动

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,11 @@
 <template>
     <div class="h-full bg-gray-50">
         <!-- 主要内容 -->
+        <div class="w-full max-w-4xl mx-auto">
+
         <main class="max-w-3xl mx-auto p-8 flex-grow">
             <slot />
         </main>
+        </div>
     </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -17,10 +17,23 @@ export default defineNuxtConfig({
     ],
 
     compatibilityDate: "2025-04-21",
+    app: {
+        head: {
+            title: "减脂计算",
+            link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+            meta: [
+                { charset: "utf-8" },
+                {
+                    name: "viewport",
+                    content:
+                        "width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no",
+                },
+            ],
+        },
+    },
     // 开发服务器配置
     devServer: {
         port: 7812, // 服务器端口
-		host: '0.0.0.0', // This allows access from any IP
+        host: "0.0.0.0", // This allows access from any IP
     },
-	
 });
